f3ee21a01705f90762771c2e8eda4b494ca656a4
azebkidanemariam <azeb.kidanemariam2@gmail.com>
2022-03-28 11:43:39 +0200

    add router

---
+++ pages/Home.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import NextImage from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.container_one}>
+          <text className={styles.text_one}>Skaffa 24gossip</text>
+
+          <text className={styles.text_two}>
+            Gå med nu och få 30 dagara grattis och efter det för en kostnad på
+            99 kr / Månaden
+          </text>
+          <button className={styles.button} type="button">
+            SKAFFA 24GOSSIP
+          </button>
+        </div>
+        <div className={styles.container_two}>
+          <NextImage src="/iphone.png" width={800} height={400} />
+        </div>
+      </main>
+    </div>
+  );
+};
+export default Home;
