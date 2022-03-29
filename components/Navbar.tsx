2bfdd4c089d22557ebf6e0a59c2d3f9160b600ba
azebkidanemariam <azeb.kidanemariam2@gmail.com>
2022-03-29 10:03:38 +0200

    add logo,delete icons,change background color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,43 +2,25 @@ import {
     Box,
     List,
     ListItem,
-    ListIcon,
-    Divider,
     LinkBox,
     LinkOverlay,
   } from "@chakra-ui/layout";
   import NextImage from "next/image";
-  import {
-    MdHome,
-    /*   MdSearch,
-    MdLibraryMusic, */
-    MdFavorite,
-    /* MdContacts, */
-    MdChatBubbleOutline,
-    MdImageSearch,
-  } from "react-icons/md";
   import NextLink from "next/link";
+
   
   const navMenu = [
     {
       name: "Hem",
-      icon: MdHome,
       route: "/Home",
     },
     {
       name: "Om oss",
-      icon: MdImageSearch,
       route: "/About",
     },
     {
       name: "Kontact",
-      icon: MdChatBubbleOutline,
       route: "/Contact",
-    },
-    {
-      name: "Favoriter",
-      icon: MdFavorite,
-      route: "/Favorites",
     },
   ];
   
@@ -47,14 +29,13 @@ import {
       <Box
         width="100%"
         height="100vh"
-        bg="black"
-        /* bg="radial-gradient(#f992b7, #fcbfdb)" */
+        bg="radial-gradient(#f992b7, #fcbfdb)"
         paddingX="5px"
-        color="gray"
+        color="white"
       >
         <Box paddingY="20px">
           <Box width="120px" marginBottom="20px" paddingX="20px">
-            <NextImage src="/logo.png" width={120} height={60} />
+            <NextImage src="/logo.svg" width={120} height={60} />
           </Box>
           <Box marginBottom="20px">
             <List spacing={2}>
@@ -63,11 +44,6 @@ import {
                   <LinkBox>
                     <NextLink href={menu.route} passHref>
                       <LinkOverlay>
-                        <ListIcon
-                          as={menu.icon}
-                          color="white"
-                          marginRight="20px"
-                        />
                         {menu.name}
                       </LinkOverlay>
                     </NextLink>
@@ -76,7 +52,6 @@ import {
               ))}
             </List>
           </Box>
-          <Divider color="gray.800" />
         </Box>
       </Box>
     );
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -18,7 +18,7 @@ const Home = () => {
           </button>
         </div>
         <div className={styles.container_two}>
-          <NextImage src="/iphone.png" width={800} height={400} />
+          <NextImage src="/iphone.png" width={400} height={300} />
         </div>
       </main>
     </div>
