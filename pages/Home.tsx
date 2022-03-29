10ac67842035b771785a9377ea88ceee05daacf8
azebkidanemariam <azeb.kidanemariam2@gmail.com>
2022-03-29 10:21:34 +0200

    small fix

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -18,7 +18,7 @@ const Home = () => {
           </button>
         </div>
         <div className={styles.container_two}>
-          <NextImage src="/iphone.png" width={400} height={300} />
+          <NextImage src="/iphone.png" width={800} height={50} />
         </div>
       </main>
     </div>
